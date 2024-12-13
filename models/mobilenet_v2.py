c9f7cef0677f6fa1665ba613feea69758da97fab
Qinkai Xu <284612606@qq.com>
2024-12-14 02:13:43 +0800

    learn code

--- models/mobilenet_v2.py
+++ models/mobilenet_v2.py
@@ -13,52 +13,83 @@ __all__ = ["MobileNetV2"]
 
 
 def conv_bn(inp, oup, stride):
+    """
+    用于创建一个包含卷积层、批归一化层和激活函数的序列模块。
+
+    Args:
+        inp: 输入通道数
+        oup: 输出通道数
+        stride: 步长
+    """
     return nn.Sequential(
         nn.Conv2d(inp, oup, 3, stride, 1, bias=False), nn.BatchNorm2d(oup), nn.ReLU6(inplace=True)
     )
 
 
 def conv_1x1_bn(inp, oup):
+    """
+    用于创建一个包含1x1卷积层、批归一化层和激活函数的序列模块。
+
+    Args:
+        inp: 输入通道数
+        oup: 输出通道数
+    """
     return nn.Sequential(
         nn.Conv2d(inp, oup, 1, 1, 0, bias=False), nn.BatchNorm2d(oup), nn.ReLU6(inplace=True)
     )
 
 
 class InvertedResidual(nn.Module):
+    """
+    实现了MobileNetV2中的倒置残差块，用于构建网络的核心模块。
+    """
     def __init__(self, inp, oup, stride, expand_ratio):
+        """
+        Args:
+            inp: 输入通道数
+            oup: 输出通道数
+            stride: 步长
+            expand_ratio: 扩展比例，用于增加或减少中间层的通道数
+        """
         super(InvertedResidual, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
 
+        # 计算中间层的通道数
         hidden_dim = round(inp * expand_ratio)
+        # 判断是否使用残差连接
         self.use_res_connect = self.stride == 1 and inp == oup
 
+        # 构建卷积序列
         if expand_ratio == 1:
             self.conv = nn.Sequential(
                 # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False), # Depthwise卷积
                 nn.BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
                 # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),                                # Pointwise卷积
                 nn.BatchNorm2d(oup),
             )
         else:
             self.conv = nn.Sequential(
                 # pw
-                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),                                # Pointwise卷积
                 nn.BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
                 # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False), # Depthwise卷积
                 nn.BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
                 # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),                                # Pointwise卷积
                 nn.BatchNorm2d(oup),
             )
 
     def forward(self, x):
+        """
+        前向传播方法。
+        """
         if self.use_res_connect:
             return x + self.conv(x)
         else:
@@ -66,12 +97,23 @@ class InvertedResidual(nn.Module):
 
 
 class MobileNetV2(nn.Module):
+    """
+    实现了MobileNetV2魔性的整体结构，用于图像分类任务。
+    """
     def __init__(self, n_class=1000, input_size=224, width_mult=1.0, dropout=0.0):
+        """
+        Args:
+            n_class: 类别数
+            input_size: 输入尺寸
+            width_mult: 宽度倍数，用于调整网络宽度
+            dropout: Dropout概率
+        """
         super().__init__()
-        block = InvertedResidual
-        input_channel = 32
-        last_channel = 1280
-        inverted_residual_setting = [
+        # 模块定义
+        block = InvertedResidual        # 基本块
+        input_channel = 32              # 初始输入通道数
+        last_channel = 1280             # 最后一层的通道数
+        inverted_residual_setting = [   # 每一层倒置残差块的配置
             # t, c, n, s
             [1, 16, 1, 1],
             [6, 24, 2, 2],
@@ -83,11 +125,16 @@ class MobileNetV2(nn.Module):
         ]
 
         # building first layer
+        # 构建第一层
         assert input_size % 32 == 0
+        # 调整初始输入通道数
         input_channel = int(input_channel * width_mult)
+        # 调整最后一层的通道数
         self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        # 添加第一层卷积模块
         features = [conv_bn(3, input_channel, 2)]
         # building inverted residual blocks
+        # 构建倒置残差块
         for t, c, n, s in inverted_residual_setting:
             output_channel = int(c * width_mult)
             for i in range(n):
@@ -97,30 +144,39 @@ class MobileNetV2(nn.Module):
                     features.append(block(input_channel, output_channel, 1, expand_ratio=t))
                 input_channel = output_channel
         # building last several layers
-        features.append(conv_1x1_bn(input_channel, self.last_channel))
-        features.append(nn.AvgPool2d(input_size // 32))
+        # 构建最后几层
+        features.append(conv_1x1_bn(input_channel, self.last_channel))  # 添加1x1卷积模块
+        features.append(nn.AvgPool2d(input_size // 32))                 # 添加全局平均池化层
         # make it nn.Sequential
-        self.features = nn.Sequential(*features)
+        self.features = nn.Sequential(*features)                        # 封装特征层
 
         # building classifier
+        # 构建分类器
         self.classifier = nn.Sequential(
             nn.Dropout(dropout),
             nn.Linear(self.last_channel, n_class),
         )
 
+        # 初始化权重
         self._initialize_weights()
 
     def forward(self, x):
+        """
+        前向传播方法。
+        """
         x = self.features(x)
         x = F.adaptive_avg_pool2d(x, 1).squeeze()  # type: ignore[arg-type] # accepted slang
         x = self.classifier(x)
         return x
 
     def _initialize_weights(self):
+        """
+        权重初始化方法。
+        """
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))    # 符合He初始化方法
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
--- models/mobilenet_v2_quantized.py
+++ models/mobilenet_v2_quantized.py
@@ -13,25 +13,48 @@ from quantization.base_quantized_model import QuantizedModel
 
 
 class QuantizedInvertedResidual(QuantizedActivation):
+    """
+    继承自`QuantizedActivation`，用于量化MobileNet中的倒置残差块。
+    """
     def __init__(self, inv_res_orig, **quant_params):
+        """
+        Args:
+            inv_res_orig: 原始的倒置残差块
+        """
         super().__init__(**quant_params)
+        # 决定是否使用残差连接
         self.use_res_connect = inv_res_orig.use_res_connect
+        # 量化卷积层
         self.conv = quantize_sequential(inv_res_orig.conv, **quant_params)
 
     def forward(self, x):
+        """
+        前向传播方法。
+        """
         if self.use_res_connect:
             x = x + self.conv(x)
-            return self.quantize_activations(x)
+            return self.quantize_activations(x)     # 对相加后的结果进行激活量化
         else:
             return self.conv(x)
 
 
 class QuantizedMobileNetV2(QuantizedModel):
+    """
+    继承自`QuantizedModel`，用于量化MobileNetV2模型。
+    """
     def __init__(self, model_fp, input_size=(1, 3, 224, 224), quant_setup=None, **quant_params):
+        """
+        Args:
+            model_fp: 原始的MobileNetV2模型
+            input_size: 输入尺寸
+            quant_setup: 量化设置的类型，决定具体的量化策略
+        """
         super().__init__(input_size)
+        # 定义特殊的量化层
         specials = {InvertedResidual: QuantizedInvertedResidual}
         # quantize and copy parts from original model
-        quantize_input = quant_setup and quant_setup == "LSQ_paper"
+        # 量化模型的特征提取部分
+        quantize_input = quant_setup and quant_setup == "LSQ_paper"     # 判断是否使用LSQ_paper量化策略
         self.features = quantize_sequential(
             model_fp.features,
             tie_activation_quantizers=not quantize_input,
@@ -39,16 +62,20 @@ class QuantizedMobileNetV2(QuantizedModel):
             **quant_params,
         )
 
+        # 量化模型的分类器部分
         self.flattener = Flattener()
         self.classifier = quantize_model(model_fp.classifier, **quant_params)
 
+        # 量化设置`FP_logits`表示不量化最后的全连接层
         if quant_setup == "FP_logits":
             print("Do not quantize output of FC layer")
             self.classifier[1].activation_quantizer = FP32Acts()
             # self.classifier.activation_quantizer = FP32Acts()  # no activation quantization of logits
+        # 量化设置`fc4`表示全连接层使用4位量化，第一个卷积层使用8位量化
         elif quant_setup == "fc4":
             self.features[0][0].weight_quantizer.quantizer.n_bits = 8
             self.classifier[1].weight_quantizer.quantizer.n_bits = 4
+        # 量化设置`fc4_dw8`表示全连接层使用4位量化，深度可分离卷积层使用8位量化，第一个卷积层使用8位量化
         elif quant_setup == "fc4_dw8":
             print("\n\n### fc4_dw8 setup ###\n\n")
             # FC layer in 4 bits, depth-wise separable once in 8 bit
@@ -58,6 +85,7 @@ class QuantizedMobileNetV2(QuantizedModel):
                 if isinstance(module, BNQConv) and module.groups == module.in_channels:
                     module.weight_quantizer.quantizer.n_bits = 8
                     print(f"Set layer {name} to 8 bits")
+        # 量化设置`LSQ`表示第一个和最后一个卷积层使用8位量化，全连接层权重使用8位量化，分类器激活值不量化
         elif quant_setup == "LSQ":
             print("Set quantization to LSQ (first+last layer in 8 bits)")
             # Weights of the first layer
@@ -68,6 +96,7 @@ class QuantizedMobileNetV2(QuantizedModel):
             self.classifier[1].weight_quantizer.quantizer.n_bits = 8
             # no activation quantization of logits
             self.classifier[1].activation_quantizer = FP32Acts()
+        # 量化设置`LSQ_paper`表示第一层卷积层使用8位量化，全连接层权重使用8位量化，不对特征提取层中的激活值进行量化
         elif quant_setup == "LSQ_paper":
             # Weights of the first layer
             self.features[0][0].activation_quantizer = FP32Acts()
@@ -79,12 +108,16 @@ class QuantizedMobileNetV2(QuantizedModel):
             for layer in self.features.modules():
                 if isinstance(layer, QuantizedActivation):
                     layer.activation_quantizer = FP32Acts()
+        # 其他量化设置
         elif quant_setup is not None and quant_setup != "all":
             raise ValueError(
                 "Quantization setup '{}' not supported for MobilenetV2".format(quant_setup)
             )
 
     def forward(self, x):
+        """
+        前向传播方法。
+        """
         x = self.features(x)
         x = self.flattener(x)
         x = self.classifier(x)
@@ -93,14 +126,26 @@ class QuantizedMobileNetV2(QuantizedModel):
 
 
 def mobilenetv2_quantized(pretrained=True, model_dir=None, load_type="fp32", **qparams):
+    """
+    用于创建和加载量化后的MobileNetV2模型。
+
+    Args:
+        pretrained: 是否使用预训练的模型，默认为True
+        model_dir: 模型的路径
+        load_type: 加载模型的类型，可以是"fp32"或"quantized"
+    """
+    # 初始化浮点模型实例
     fp_model = MobileNetV2()
+    # 加载预训练的FP32模型
     if pretrained and load_type == "fp32":
         # Load model from pretrained FP32 weights
         assert os.path.exists(model_dir)
         print(f"Loading pretrained weights from {model_dir}")
         state_dict = torch.load(model_dir)
         fp_model.load_state_dict(state_dict)
+        # 量化模型
         quant_model = QuantizedMobileNetV2(fp_model, **qparams)
+    # 加载量化模型
     elif load_type == "quantized":
         # Load pretrained QuantizedModel
         print(f"Loading pretrained quantized model from {model_dir}")
